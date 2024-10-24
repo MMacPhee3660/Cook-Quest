25dc2ac4bfbe59988c25c2fca687f9b74ca12d09
Liam <lvogel3105@student.pps.net>
2024-10-24 11:55:32 -0700

    Scene transition
    In Progress Close to Done

--- Assets/Scriptable Objects/SceneInfo.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "SceneInfo", menuName = "Persistence")]
-public class SceneInfo : ScritpableObject {
-    public bool isNextScene = true;
-}
-
--- Assets/Scripts/SceneInfo.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class SceneInfo : ScriptableObject {
-    [CreateAssetMenu(fileName = "SceneInfo", menuName = "Persistence")]
-    public bool isNextScene = true;
-}
-
