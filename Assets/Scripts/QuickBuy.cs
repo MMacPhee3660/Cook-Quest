7b24fbe627dc5cd70624f2b5d137402ae999f8e4
mmacphee3660 <mmacphee3660@student.pps.net>
2024-10-30 10:53:21 -0700

    test

--- Assets/Scripts/QuickBuy.cs
+++ Assets/Scripts/QuickBuy.cs
@@ -1,26 +1,27 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class QuickBuy : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     public GameObject Capsule
-//     public GameObject E
-    
-    
-    
-//     void Start()
-//     {
-//         E.disable()
-//         if Capsule.trasform position < 10f{
-//             E.enable()
-//         }
-//     }
+public class QuickBuy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Capsule
+    public GameObject E
+    public GameObject "Lower Left Object"
+    float dist = Vector3.Distance(Capsule.position, "Lower Left Object".position);
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.E))
-//     }
-// }
+    void Start()
+    {
+        print(dist)
+        // E.disable()
+        // if Capsule.trasform position < 10f{
+        //     E.enable()
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+    }
+}
