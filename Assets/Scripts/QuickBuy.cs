79bb286097328f6f2624bfb91db7ea0347cc5baf
mmacphee3660 <mmacphee3660@student.pps.net>
2024-10-30 10:39:05 -0700

    Stuff

---
+++ Assets/Scripts/QuickBuy.cs
@@ -0,0 +1,26 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class QuickBuy : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public GameObject Capsule
+//     public GameObject E
+    
+    
+    
+//     void Start()
+//     {
+//         E.disable()
+//         if Capsule.trasform position < 10f{
+//             E.enable()
+//         }
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if(Input.GetKeyDown(KeyCode.E))
+//     }
+// }
