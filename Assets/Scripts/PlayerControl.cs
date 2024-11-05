e31c558d12bf8e1e5a01fb8bc61ede1bc0557e83
Oscar Hoffelt <ohoffelt4013@student.pps.net>
2024-11-05 11:22:05 -0800

    working dash
    feel free to tune

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
-    
     [SerializeField] float speed = 5f;
     public float baseSpeed = 5f;
 
@@ -15,23 +13,29 @@ public class PlayerController : MonoBehaviour
     public float sprintingMulti = 1.5f;
     public bool slowWalk;
     public bool isDash = false;
+    private bool dashComplete = false;
+    private float timeSinceDash = 0.5f;
 
-
-   void Start(){
-        rb = GetComponent<Rigidbody>();
-   }
     void FixedUpdate()
     {
+        //Debug.Log(speed);
         float horzInput = Input.GetAxisRaw("Horizontal");
         float vertInput = Input.GetAxisRaw("Vertical"); 
 
     
-         Vector3 movement = new(horzInput, 0, vertInput);
-         movement.Normalize();
+        Vector3 input = new(horzInput, 0, vertInput);
+        input.Normalize();
 
-         rb.MovePosition(rb.position + movement * Time.fixedDeltaTime * speed);
-         transform.forward = new Vector3(movement.x,0,movement.z);
-         if( Input.GetKey(KeyCode.LeftShift)){
+        if (isDash){
+            Dash();
+            transform.position += (transform.forward * speed * Time.deltaTime);
+        }
+        else if (horzInput != 0 || vertInput != 0){
+            transform.forward = new Vector3(input.x,0,input.z);
+            transform.position += (input * Time.fixedDeltaTime * speed);
+        }
+        
+        if( Input.GetKey(KeyCode.LeftShift)){
             isSprinting = true;
         }
         else{
@@ -39,12 +43,12 @@ public class PlayerController : MonoBehaviour
             isSprinting = false;
         }
 
-        if(isSprinting == true) {
+        if(isSprinting) {
             speed = baseSpeed * 1.5f;
 
         }
 
-        if(isSprinting == false){
+        if(!isSprinting && !isDash){
             speed = baseSpeed;
         }
 
@@ -55,14 +59,37 @@ public class PlayerController : MonoBehaviour
             baseSpeed = 5f;
         }
 
-        if(Input.GetKey(KeyCode.Space)){
+        if(Input.GetKey(KeyCode.Space) && timeSinceDash >= 0.15){
             isDash = true;
-
+            dashComplete = false;
         }
-
-
+        if (timeSinceDash > 0.15){
+            slowWalk = false;
+        }
+        else{
+            timeSinceDash += Time.deltaTime;
+        }
     }
-    
+    private void Dash(){
+        int dashAcceleration = 300;
+        int dashSpeed = 35;
+        timeSinceDash = 0;
+        if (speed < dashSpeed && !dashComplete){
+            speed += Time.deltaTime * dashAcceleration;
+        }
+        else {
+            dashComplete = true;
+        }
+        if (dashComplete){
+            if (speed > 0){
+                speed -= Time.deltaTime * dashAcceleration;
+            }
+            else {
+                isDash = false;
+                slowWalk = true;
+            }
+        }
+    }
     
     
 }
