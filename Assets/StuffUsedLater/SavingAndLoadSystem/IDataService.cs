892700fb2252084c4505af4feb3c53f4534a772f
Liam <lvogel3105@student.pps.net>
2024-11-20 11:57:44 -0800

    Started working on the Save and Loading System

---
+++ Assets/StuffUsedLater/SavingAndLoadSystem/IDataService.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+public interface IDataService
+{
+    bool SaveData<T>(string RelativePath, T Data, bool Encrypted);
+
+    T LoadData<T>(string RelativePath, bool Encrypted);
+}
