cf507d31e2794c218b1027ef88b63bb04aed6dbf
mmacphee3660 <mmacphee3660@student.pps.net>
2024-12-09 11:40:22 -0800

    select items in menu WIP

--- Assets/Scripts/MenuBuy.cs
+++ Assets/Scripts/MenuBuy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using TMPro;
 
 public class Menubuy : MonoBehaviour
 {
@@ -13,12 +14,16 @@ public class Menubuy : MonoBehaviour
     public GameObject testE;
     public GameObject childE;
     public GameObject menu;
+    public GameObject menuObject;
+    private int itemCount = 0;
+    public TMP_Text countText;
     
     void Start()
     {
         player = GameObject.Find("PlayerMove");
         E.SetActive(false);
         menu.SetActive(false);
+        menuObject.SetActive(false);
         childE = Instantiate(testE, transform.position,Quaternion.identity);
         childE.transform.position += Vector3.up * 1f;
     }
@@ -26,6 +31,7 @@ public class Menubuy : MonoBehaviour
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
 
+        countText.text = itemCount + "";
 
 
         if(distance <= 2f){
@@ -42,14 +48,12 @@ public class Menubuy : MonoBehaviour
             print("a");
             if(menu.activeSelf == false && childE.activeSelf == true){
                 menu.SetActive(true);
+                menuObject.SetActive(true);
             }
             else{
                 menu.SetActive(false);
+                menuObject.SetActive(false);
             }
         }
-    }
-    void OnMouseDown()
-    {
-        print("click");
     }
 }
