035eb7400a1060f75a9c6055bf99c3e5aaa50dc9
Oscar Hoffelt <ohoffelt4013@student.pps.net>
2024-12-09 11:40:51 -0800

    pathfinding a* progress

--- Assets/Scripts/NPCPathing.cs
+++ Assets/Scripts/NPCPathing.cs
@@ -1,50 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Timeline;
 
 public class NPCPathing : MonoBehaviour
 {
-    [SerializeField] UnityEngine.Vector3 destination1 = new UnityEngine.Vector3(0,0,0);
-    [SerializeField] UnityEngine.Vector3 destination2 = new UnityEngine.Vector3(0,0,0);
-    [SerializeField] UnityEngine.Vector3 destination3 = new UnityEngine.Vector3(0,0,0);
-    List<UnityEngine.Vector3> nodes = new List<UnityEngine.Vector3>();
-    bool nodeReached = false;
-    int frameCount = 0;
-    int currentNode = 0;
-    float timeReached;
-    UnityEngine.Vector3 pos;
+    [SerializeField] private int gridHeight = 10;
+    [SerializeField] private int gridWidth = 10;
+    [SerializeField] private float cellHeight = 1f;
+    [SerializeField] private float cellWidth = 1f;
 
-    void Update()
+    [SerializeField] private bool generatePath;
+    [SerializeField] private bool visualizeGrid;
+    
+    private bool pathGenerated;
+
+    private Dictionary<Vector2, Cell> cells;
+
+    [SerializeField] private List<Vector2> cellsToSearch;
+    [SerializeField] private List<Vector2> searchedCells;
+    [SerializeField] private List<Vector2> finalPath;
+
+    private void Update()
     {
-        if (frameCount == 0)
+        if (generatePath && !pathGenerated)
         {
-            nodes.AddRange(new UnityEngine.Vector3[]{destination1,destination2,destination3});
-            pos = transform.position;
+            GenerateGrid();
+            pathGenerated = true;
         }
-        if (!nodeReached)
+        else if (!generatePath)
         {
-            if (transform.position.Equals(nodes[currentNode]))
+            pathGenerated = false;
+        }
+    }
+    private void GenerateGrid()
+    {
+        cells = new Dictionary<Vector2, Cell>();
+
+        for (float x = 0; x < gridWidth; x += cellWidth)
+        {
+            for (float y = 0; y < gridHeight; y += cellHeight)
             {
-                nodeReached = true;
-                timeReached = Time.time;
+                Vector2 pos = new Vector2(x,y);
+                cells.Add(pos, new Cell(pos));
+            }
+        }
+        
+        for (int i = 0; i < 40; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(0, gridWidth), Random.Range(0, gridHeight));
+            cells[pos].isWall = true;
+        }
+    }
+
+    private void FindPath(Vector2 startPos, Vector2 endPos)
+    {
+        searchedCells = new List<Vector2>();
+        cellsToSearch = new List<Vector2> {startPos};
+        finalPath = new List<Vector2>();
+
+        Cell startCell = cells[startPos];
+        startCell.gCost = 0;
+        startCell.hCost = GetDistance(startPos, endPos);
+        startCell.fCost = GetDistance(startPos, endPos);
+
+        while (cellsToSearch.Count > 0)
+        {
+            
+        }
+    }
+
+    private int GetDistance(Vector2 pos1, Vector2 pos2)
+    {
+        Vector2Int dist = new Vector2Int(Mathf.Abs((int)pos1.x - (int)pos2.x), Mathf.Abs((int)pos1.y - (int)pos2.y));
+
+        int lowest = Mathf.Min(dist.x, dist.y);
+        int highest = Mathf.Max(dist.x, dist.y);
+
+        int horizontalMovesRequired = highest - lowest;
+
+        return lowest * 14 + horizontalMovesRequired * 10;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!visualizeGrid || cells == null)
+        {
+            return;
+        }
+        foreach (KeyValuePair<Vector2, Cell> kvp in cells)
+        {
+            if (!kvp.Value.isWall)
+            {
+                Gizmos.color = Color.white;
             }
             else
             {
-                transform.position = UnityEngine.Vector3.MoveTowards(pos,nodes[currentNode],Time.deltaTime);
+                Gizmos.color = Color.black;
             }
+
+            Gizmos.DrawCube(kvp.Key + (Vector2)transform.position, new Vector3(cellWidth,cellHeight));
         }
-        else if (Time.time >= timeReached + 2)
-        {
-            nodeReached = false;
-            pos = transform.position;
-            currentNode++;
-        }
-        frameCount++;
     }
 
-       
+    private class Cell
+    {
+        public Vector2 position;
+        public int fCost = int.MaxValue;
+        public int gCost = int.MaxValue;
+        public int hCost = int.MaxValue;
+        public Vector2 connection;
+        public bool isWall;
 
+        public Cell(Vector2 pos)
+        {
+            position = pos;
+        }
+    }
 }
