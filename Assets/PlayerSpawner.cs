43ccf9a992bef0db90b5850c10fd3ecec47bae99
tjohoski3165 <tjohoski3165@studnet.pps.net>
2025-01-08 12:01:08 -0800

    made spawnpoint, began scene transitions

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float X;
+    public float Y;
+    public float Z;
+    GameObject player;
+    public GameObject spawnpoint;
+    void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = new Vector3(spawnpoint.transform.position.x,spawnpoint.transform.position.y,spawnpoint.transform.position.z);
     }
 
     // Update is called once per frame
