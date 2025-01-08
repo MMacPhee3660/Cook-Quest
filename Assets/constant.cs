9a760eaf60f204e8d9aeab58b54b28322a105077
tjohoski3165 <tjohoski3165@studnet.pps.net>
2025-01-08 11:32:20 -0800

    stuff

---
+++ Assets/constant.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class constant : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
