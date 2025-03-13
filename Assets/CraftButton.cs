b2e46baf6c9823e2abba4efe0dd47870bc572659
tjohoski3165 <tjohoski3165@studnet.pps.net>
2025-03-13 11:49:57 -0700

    fixed crafting by using touples

--- Assets/CraftButton.cs
+++ Assets/CraftButton.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CraftButton : MonoBehaviour
@@ -11,6 +13,7 @@ public class CraftButton : MonoBehaviour
 
     [SerializeField] Item[] itemsReq;
     [SerializeField] int[] amounts;
+    public List<Tuple<InventoryItem, int>> slots = new List<Tuple<InventoryItem, int>>();
     
     void Start()
     {
@@ -29,25 +32,35 @@ public class CraftButton : MonoBehaviour
         foreach(Item currItem in itemsReq){
             Debug.Log(currItem);
             for(int i = 0; i < inventoryManager.InventorySlots.Length; i++){
-                
                 InventorySlot slot = inventoryManager.InventorySlots[i];
                 InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
                 if(itemInSlot != null && itemInSlot.item == currItem && itemInSlot.count >= amounts[pos]){
                     haveItems += 1;
-                    itemInSlot.count -= amounts[pos];
-                    Debug.Log(itemInSlot.count);
-                    if(itemInSlot.count == 0){
-                        Destroy(itemInSlot.gameObject);
-                        Destroy(itemInSlot.image);
-                        
-                    }
-                   
+                    Debug.Log(haveItems);
+                    slots.Add(new Tuple<InventoryItem, int>(itemInSlot,amounts[pos]));
+                    Debug.Log("i got past");
                 }
             }
             pos += 1;
         }
         if(haveItems == itemsReq.Length){
             inventoryManager.AddItem(productItem);
+            foreach(var slot in slots){
+                slot.Item1.count -= slot.Item2;
+                slot.Item1.RefreshCount();
+                if(slot.Item1.count <= 0){
+                    Destroy(slot.Item1.gameObject);
+                }
+            }
+            slots.Clear();
         }
     }
 }
+// itemInSlot.count -= amounts[pos];
+//                     itemInSlot.RefreshCount();
+//                     Debug.Log(itemInSlot.count);
+//                     if(itemInSlot.count <= 0){
+//                         Destroy(itemInSlot.gameObject);
+//                         Destroy(itemInSlot.image);
+                        
+//                     }
